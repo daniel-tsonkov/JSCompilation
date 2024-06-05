cbcbc78792e1343a43f7e3bd70f05a6c15a5b849
dan <daniel_tzonkov@abv.bg>
2024-06-05 09:18:35 +0300

    asd

--- 1Fruit.js
+++ 1Fruit.js
@@ -1,5 +1,7 @@
 function fruit(fruitName, weightInGrams, priceperKg) {
-    
+    let kilograms = weightInGrams / 1000;
+    let price = kilograms * priceperKg;
+    console.log(`I need $${price} to buy ${kilograms.toFixed(2)} kilograms ${fruitName}`, )
 }
 
 fruit('orange', 2500, 1.80)
