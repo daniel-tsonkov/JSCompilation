4f68df82fd86a372d99b75fbf7b0b7d57a5fd597
dan <daniel_tzonkov@abv.bg>
2024-06-06 14:00:37 +0300

    up

--- ListOfNames.js
+++ ListOfNames.js
@@ -1,13 +1,9 @@
 function solve(arr) {
-    let newArr = [];
-    let biggestNum = 0;
-
-    const sortedWords = arr.sort();
+    arr.sort((a, b) => a.localeCompare(b));
 
     for (let index = 0; index < arr.length; index++) {
-        console.log(`${index}. ${sortedWords[index]}`); 
+        console.log(`${index + 1}. ${arr[index]}`); 
     }
- 
 }
 
-solve(['John', 'Bob', 'Christina', 'Ema']);
+solve(["John", "Bob", "Christina", "Ema"]);
