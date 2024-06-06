af1864e3667cf321cbc69926042304197a4ed209
dan <daniel_tzonkov@abv.bg>
2024-06-06 11:22:45 +0300

    up

--- AddAndRemoveElements.js
+++ AddAndRemoveElements.js
@@ -1,5 +1,34 @@
-function solve(action) {
+function solve(arr) {
+    let newArr = [];
+    let num = 0;
 
+    for (let index = 0; index < arr.length; index++) {
+        num++;
+        let command = arr[index];
+        console.log(command);
+
+        switch (command) {
+            case 'add':
+                newArr.push(num);
+                break;
+
+            case 'remove':
+                newArr.pop();
+                break;
+        }
+        // if(command == "add") {
+        //     newArr.push(num);
+        // } else if(command == 'remove') {
+        //     newArr.pop();
+        // }
+    }
+
+    if (newArr.length == 0) {
+        console.log('empty');
+    } else {
+        console.log(newArr.join('\n'));
+
+    }
 }
 
-solve('add', 'add', 'add', 'add');
+solve(['add', 'add', 'remove', 'add', 'add']);
