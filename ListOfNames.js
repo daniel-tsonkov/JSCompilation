bf860921fa7fcded6968cc47f63ef9e9ef9c54a8
dan <daniel_tzonkov@abv.bg>
2024-06-06 14:26:40 +0300

    aaa

--- ListOfNames.js
+++ ListOfNames.js
@@ -1,9 +1,13 @@
 function solve(arr) {
     arr.sort((a, b) => a.localeCompare(b));
 
-    for (let index = 0; index < arr.length; index++) {
-        console.log(`${index + 1}. ${arr[index]}`); 
-    }
+    // for (let index = 0; index < arr.length; index++) { //another method
+    //     console.log(`${index + 1}. ${arr[index]}`); 
+    // }
+
+    arr.forEach((е, index) => {
+        console.log(`${index + 1}. ${е}`);
+    });
 }
 
 solve(["John", "Bob", "Christina", "Ema"]);
