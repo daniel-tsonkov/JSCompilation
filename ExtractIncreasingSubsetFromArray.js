d2a15a0d13278e2bf0b0c95bef8bd0bd59d5fdc8
dan <daniel_tzonkov@abv.bg>
2024-06-06 11:49:19 +0300

    up

---
+++ ExtractIncreasingSubsetFromArray.js
@@ -0,0 +1,17 @@
+function solve(arr) {
+    let newArr = [];
+    let biggestNum = 0;
+
+    for (let index = 0; index < arr.length; index++) {
+        let element = arr[index]; 
+
+        if (biggestNum <= element) {
+            newArr.push(element)
+            biggestNum = element;
+        }
+    }
+
+    console.log(newArr);
+}
+
+solve([1, 3, 8, 4, 10, 12, 3, 2, 24]);
