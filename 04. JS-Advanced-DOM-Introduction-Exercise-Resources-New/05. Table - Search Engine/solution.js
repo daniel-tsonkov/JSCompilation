49e78ebe2bd7dc59cc6868842c7ff4c59a3347ff
dan <daniel_tzonkov@abv.bg>
2024-06-11 14:16:35 +0300

    up

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,37 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   let rows = document.querySelectorAll('tbody tr');
+   let input = document.getElementById('searchField');
 
+
+   function onClick() {
+      for (let row of rows) {
+         row.classList.remove('select');
+
+         if (row.innerHTML.includes(input.value)) {
+            row.className = 'select';
+         }
+      }
    }
+
+   input.addEventListener('input', function(event) {
+      //console.log('Input event detected!');
+      //console.log('Current value:', event.target.value);
+      //console.log('Current value:', input.value.length); 
+      
+      for (let row of rows) {
+         row.classList.remove('select');
+
+         if (row.innerHTML.includes(input.value)) {
+            row.className = 'select';
+         }
+      }
+
+      if(input.value.length == 0){
+         for (let row of rows) {
+            row.classList.remove('select');
+         } 
+      }
+  });
 }
