7238256f6c548a6b893060a810afc63fe72381ff
dan <daniel_tzonkov@abv.bg>
2024-06-11 09:33:40 +0300

    up

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,9 +1,15 @@
 function solve() {
   let text = document.getElementById('text').value;
   let namingConvention = document.getElementById('naming-convention').value;
-  let resultContainer = document.getElementById('result-container').value;
+  //let resultContainer = document.getElementById('result-container').value;
   let result = '';
-  let words = text.split(/(\s+)/);
+  let words = text.split(/(\s+)/).filter(x => x != ' ');
+  //test string aaa
+  if (namingConvention == 'Camel Case') {
 
-  
+  } else if (namingConvention == 'Pascal Case') {
+
+  } else {
+    return document.getElementById('result').textContent = 'Error!';
+  }
 }
