ddb59f52a587b3d30f98fabb251d2a4c5e77c6b2
dan <daniel_tzonkov@abv.bg>
2024-06-11 13:19:19 +0300

    up

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   let listElements = document.querySelectorAll('ul#towns li');
+   let searchText = document.getElementById('searchText').value;
+   let maches;
+
+   for (const element of listElements) {
+      let text = element.textContent;
+      
+      element.style.fontWeight = 'normal';
+      element.style.textDecoration = '';
+
+      if (text.match(searchText)) {
+         maches++;
+         element.style.fontWeight = 'bold';
+         element.style.textDecoration = 'underline';
+      }
+   }
 }
