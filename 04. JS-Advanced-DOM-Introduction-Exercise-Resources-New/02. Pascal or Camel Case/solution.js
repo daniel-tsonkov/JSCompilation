48320fd17ec03de77f319c25ce3e7d24514bdecf
dan <daniel_tzonkov@abv.bg>
2024-06-11 09:19:18 +0300

    up

---
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -0,0 +1,9 @@
+function solve() {
+  let text = document.getElementById('text').value;
+  let namingConvention = document.getElementById('naming-convention').value;
+  let resultContainer = document.getElementById('result-container').value;
+  let result = '';
+  let words = text.split(/(\s+)/);
+
+  
+}
