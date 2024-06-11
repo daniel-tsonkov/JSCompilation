33856f4e24a631fa8c76aa18ef1b6c9922fad29e
dan <daniel_tzonkov@abv.bg>
2024-06-11 10:11:55 +0300

    up

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,15 +1,25 @@
 function solve() {
   let text = document.getElementById('text').value;
   let namingConvention = document.getElementById('naming-convention').value;
-  //let resultContainer = document.getElementById('result-container').value;
   let result = '';
   let words = text.split(/(\s+)/).filter(x => x != ' ');
   //test string aaa
   if (namingConvention == 'Camel Case') {
+    words[0] = words[0].toLowerCase();
 
+    for (let index = 1; index < words.length; index++) {
+      words[index] = words[index].toLowerCase();
+      words[index] = words[index].charAt(0).toUpperCase() + words[index].substring(1);
+    }
   } else if (namingConvention == 'Pascal Case') {
-
+    for (let index = 0; index < words.length; index++) {
+      words[index] = words[index].toLowerCase();
+      words[index] = words[index].charAt(0).toUpperCase() + words[index].substring(1);
+    }
   } else {
     return document.getElementById('result').textContent = 'Error!';
   }
+
+  result = words.join('');
+  document.getElementById('result').textContent = result;
 }
